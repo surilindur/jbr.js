00b1467960d58f8eabcadc84d2bb0fc9c343c915
surilindur <surilindur@users.noreply.github.com>
2024-04-04 10:24:25 +0300

    Fix linter errors in packages/jbr/lib/npm/NpmInstaller.ts

--- packages/jbr/lib/npm/NpmInstaller.ts
+++ packages/jbr/lib/npm/NpmInstaller.ts
@@ -1,6 +1,7 @@
 /**
  * Installs npm packages.
  */
+// eslint-disable-next-line ts/naming-convention
 export interface NpmInstaller {
   install: (cwd: string, packages: string[], scopeError: string) => Promise<void>;
 }
