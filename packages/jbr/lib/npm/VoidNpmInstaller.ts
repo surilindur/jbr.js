cd12d7fcee1240f0ed1cd2af24da7b3a506fb492
surilindur <surilindur@users.noreply.github.com>
2024-04-04 10:24:00 +0300

    Fix linter errors in packages/jbr/lib/npm/VoidNpmInstaller

--- packages/jbr/lib/npm/VoidNpmInstaller.ts
+++ packages/jbr/lib/npm/VoidNpmInstaller.ts
@@ -4,6 +4,7 @@ import type { NpmInstaller } from './NpmInstaller';
  * A dummy npm installer that does not install anything.
  */
 export class VoidNpmInstaller implements NpmInstaller {
+  // eslint-disable-next-line unused-imports/no-unused-vars
   public async install(cwd: string, packages: string[]): Promise<void> {
     // Do nothing
   }
